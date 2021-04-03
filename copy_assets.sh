d4497d30c8bdf92639c193b19f8aadcb8444c794
Marcelo Almeida <mgba@umich.edu>
2021-04-03 01:06:01 -0400

    updated code_examples files

--- copy_assets.sh
+++ copy_assets.sh
@@ -3,7 +3,7 @@
 # This is a convenience script to copy the code examples and corresponding SVG
 # visualizations from the rustviz repository. See README.md for more details.
 
-for DIR in ../rustviz/svg_generator/examples/*/; do
+for DIR in ../rustviz/dsl/examples_dsl/*/; do
     BASENAME=`basename $DIR`
     if [[ -f  "$DIR/source.rs" && -f "$DIR/vis_code.svg" && -f "$DIR/vis_timeline.svg" ]]
     then
@@ -17,3 +17,19 @@ for DIR in ../rustviz/svg_generator/examples/*/; do
         echo "$BASENAME does not have the required files, skipping."
     fi
 done
+
+# # Copy new files to modified_examples
+# DSL='../rustviz/dsl/examples_dsl'
+# for DIR in ./src/assets/modified_examples/*; do
+#     BASENAME=`basename $DIR`
+#     if [[ -f  "$DSL/$BASENAME/source.rs" && -f "$DSL/$BASENAME/vis_code.svg" && -f "$DSL/$BASENAME/vis_timeline.svg" ]]
+#     then
+#         cp "$DSL/$BASENAME/source.rs" "$DIR/source.rs" && \
+#         cp "$DSL/$BASENAME/vis_code.svg" "$DIR/vis_code.svg" && \
+#         cp "$DSL/$BASENAME/vis_timeline.svg" "$DIR/vis_timeline.svg"
+        
+#         echo "Successfully copied $BASENAME."
+#     else
+#         echo "$BASENAME does not have the required files, skipping."
+#     fi
+# done
